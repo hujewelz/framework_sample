a21d55092650940932226d158d9807077c789c44
huluobo <hujewelz@163.com>
2018-06-21 15:35:32 +0800

    initial commit

---
+++ B/B/FrameworkB.h
@@ -0,0 +1,15 @@
+//
+//  FrameworkB.h
+//  B
+//
+//  Created by huluobo on 2018/5/14.
+//  Copyright © 2018年 jewelz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrameworkB : NSObject
+
++ (void)test;
+
+@end
---
+++ B/B/FrameworkB.m
@@ -0,0 +1,17 @@
+//
+//  FrameworkB.m
+//  B
+//
+//  Created by huluobo on 2018/5/14.
+//  Copyright © 2018年 jewelz. All rights reserved.
+//
+
+#import "FrameworkB.h"
+
+@implementation FrameworkB
+
++ (void)test {
+    NSLog(@"test from framework B!");
+}
+
+@end
